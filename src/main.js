d99fd4ecd757ee695bd648dd5d2b7a9e03577325
mattialucca92 <mattialucca92@gmail.com>
2025-05-27 22:37:19 +0200

    inizializzazione progetto

---
+++ src/main.js
@@ -0,0 +1,113 @@
+import './style.css'
+
+
+const calciatori = [
+  {
+    nome: "Lionel Messi",
+    squadra: "Inter Miami",
+    nazionalità: "Argentina",
+    ruolo: "Attaccante",
+    immagine: "/img/messi.jpg",
+  },
+  {
+    nome: "Cristiano Ronaldo",
+    squadra: "Al-Nassr",
+    nazionalità: "Portogallo",
+    ruolo: "Attaccante",
+    immagine: "/img/ronaldo.jpg",
+  },
+  {
+    nome: "Kylian Mbappé",
+    squadra: "Paris Saint-Germain",
+    nazionalità: "Francia",
+    ruolo: "Attaccante",
+    immagine: "/img/mbappe.jpg",
+  },
+  {
+    nome: "Erling Haaland",
+    squadra: "Manchester City",
+    nazionalità: "Norvegia",
+    ruolo: "Attaccante",
+    immagine: "/img/haaland.jpg",
+  },
+  {
+    nome: "Kevin De Bruyne",
+    squadra: "Manchester City",
+    nazionalità: "Belgio",
+    ruolo: "Centrocampista",
+    immagine: "/img/debruyne.jpg",
+  },
+  {
+    nome: "Luka Modrić",
+    squadra: "Real Madrid",
+    nazionalità: "Croazia",
+    ruolo: "Centrocampista",
+    immagine: "/img/modric.jpg",
+  },
+  {
+    nome: "Robert Lewandowski",
+    squadra: "Barcellona",
+    nazionalità: "Polonia",
+    ruolo: "Attaccante",
+    immagine: "/img/lewandowski.jpg",
+  },
+  {
+    nome: "Harry Kane",
+    squadra: "Bayern Monaco",
+    nazionalità: "Inghilterra",
+    ruolo: "Attaccante",
+    immagine: "/img/kane.jpg",
+  },
+  {
+    nome: "Vinícius Júnior",
+    squadra: "Real Madrid",
+    nazionalità: "Brasile",
+    ruolo: "Attaccante",
+    immagine: "/img/vinicius.jpg",
+  },
+  {
+    nome: "Neymar",
+    squadra: "Al Hilal",
+    nazionalità: "Brasile",
+    ruolo: "Attaccante",
+    immagine: "/img/neymar.jpg",
+  },
+  {
+    nome: "Pedri",
+    squadra: "Barcellona",
+    nazionalità: "Spagna",
+    ruolo: "Centrocampista",
+    immagine: "/img/pedri.jpg",
+  },
+  {
+    nome: "Jude Bellingham",
+    squadra: "Real Madrid",
+    nazionalità: "Inghilterra",
+    ruolo: "Centrocampista",
+    immagine: "/img/bellingham.jpg",
+  },
+  {
+    nome: "Sandro Tonali",
+    squadra: "Newcastle",
+    nazionalità: "Italia",
+    ruolo: "Centrocampista",
+    immagine: "/img/tonali.jpg",
+  },
+  {
+    nome: "Victor Osimhen",
+    squadra: "Napoli",
+    nazionalità: "Nigeria",
+    ruolo: "Attaccante",
+    immagine: "/img/osimhen.jpg",
+  },
+  {
+    nome: "Gianluigi Donnarumma",
+    squadra: "Paris Saint-Germain",
+    nazionalità: "Italia",
+    ruolo: "Portiere",
+    immagine: "/img/donnarumma.jpg",
+  },
+];
+
+
+
