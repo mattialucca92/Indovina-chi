2ce476cefeeafb93d7a7133c8a1b79903c3a361b
mattialucca92 <mattialucca92@gmail.com>
2025-06-13 15:53:32 +0200

    creazione cartella docs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: "/Indovina-chi/", // ✅ questo va bene per GitHub Pages!
+  base: "/Indovina-chi/", // Nome del tuo repository
+  build: {
+    outDir: "docs", // GitHub Pages legge da /docs
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });
